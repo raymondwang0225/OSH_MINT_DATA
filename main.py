855eabed228bf1ced7fc9c9343bfa680c9aab913
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-22 05:38:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-1
+import streamlit as st
+from typing import List
+from dataclasses import dataclass
+from itertools import product
+import json;
+from PIL import Image
+import requests
+import pandas as pd
+import numpy as np
+import csv
+import plost
+import matplotlib.pyplot as plt
+import altair as alt
+
+
+st.set_page_config(layout="wide")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+st.title("OSH Holders")
+
+
+OSH_LIST_HOLDERS =pd.read_csv('OSH_LIST_HOLDERS_CSV')
+
+st.table(OSH_LIST_HOLDERS)
